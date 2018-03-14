27ffe9920214e081044d00e1292c776f19ddbf79
mihaimatraguna <matragunamihai97@gmail.com>
2018-03-14 13:25:19 +0000

    Add the GUI class

---
+++ src/GUI/GUI.java
@@ -0,0 +1,12 @@
+package GUI;
+
+import javax.swing.*;
+
+/**
+ * In this class we will manage the GUI of our application.
+ * @author Mihai
+ */
+public class GUI {
+    private JPanel panel1;
+    private JTextField textField1;
+}
