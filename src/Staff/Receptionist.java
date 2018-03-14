f9a282ee8e6de8f4edc3a32f77e9d3cd91eda1ca
mihaimatraguna <matragunamihai97@gmail.com>
2018-03-14 15:12:46 +0000

    Added attributes and functions of Staff package. The functions are incomplete, they have just the headers.

--- src/Staff/Receptionist.java
+++ src/Staff/Receptionist.java
@@ -1,5 +1,42 @@
 package Staff;
 
 
-public class Receptionist extends Staff{
+public class Receptionist extends Staff {
+
+    public Receptionist(int accountNo, String firstName, String lastName, String address, String phone) {
+        super(accountNo, firstName, lastName, address, phone);
+    }
+
+    private void searchCustomer(String holderName, String contactName, String address, String phone){
+
+    }
+
+    private void createCustomerAccount(String holderName, String contactName, String address, String phone){
+
+    }
+
+    private void printLabel(String labelName) {
+
+    }
+
+    private void printReciept(){
+
+    }
+
+    private void recordDeadline(){
+
+    }
+
+    private void alertShiftManager(String message){
+
+    }
+
+    private boolean createJob(String specialInstructions, String customerId){
+        return true;
+    }
+
+    private void recordInstructions(String jobID){
+
+    }
+
 }
--- src/Staff/ShiftManager.java
+++ src/Staff/ShiftManager.java
@@ -2,6 +2,43 @@ package Staff;
 
 /**
  * Represents ShiftManager class
+ * @author Mihai
  */
 public class ShiftManager extends Staff {
+
+    public ShiftManager(int accountNo, String firstName, String lastName, String address, String phone) {
+        super(accountNo, firstName, lastName, address, phone);
+    }
+
+    private void searchCustomer(String holderName, String contactName, String address, String phone){
+
+    }
+
+    private void createCustomerAccount(String holderName, String contactName, String address, String phone){
+
+    }
+
+    private void printLabel(String labelName) {
+
+    }
+
+    private void printReciept(){
+
+    }
+
+    private void recordDeadline(){
+
+    }
+
+    private void alertShiftManager(String message){
+
+    }
+
+    private boolean createJob(String specialInstructions, String customerId){
+        return true;
+    }
+
+    private void recordInstructions(String jobID){
+
+    }
 }
