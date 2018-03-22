163498eabab90f1fa872fb74f6983e9e350ac241
Elmar Talibzade <elmar.talibzade@gmail.com>
2018-03-22 17:31:10 +0000

    Added staff roles to be able to differentiate a staff role

---
+++ src/Staff/Role.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Staff;
+
+/**
+ * Type of roles in the company
+ * @author Elmar Talibzade
+ */
+public enum Role {
+    
+    /**
+     * Office Manager
+     */
+    OfficeManager,
+    
+    /**
+     * Shift Manager 
+     */
+    ShiftManager,
+    
+    /**
+     * Technician
+     */
+    Technician,
+    
+    /**
+     * Receptionist
+     */
+    Receptionist
+    
+}
