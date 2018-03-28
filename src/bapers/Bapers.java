bf6f814af2e0ef841be16ed188d7512c232887e4
Elmar Talibzade <elmar.talibzade@gmail.com>
2018-03-28 16:25:19 +0100

    Added GUI Panels

--- src/bapers/Bapers.java
+++ src/bapers/Bapers.java
@@ -6,12 +6,8 @@
 package bapers;
 
 import Administrator.*;
-import Customer.Customer;
-import Customer.Job;
-import Customer.Task;
-import GUI.LoginUI;
-import java.util.ArrayList;
-
+import GUI.*;
+import javax.swing.UIManager;
 /**
  * Main class for running BAPERS software.
  *
@@ -23,22 +19,49 @@ public class Bapers {
      * Sole connection to the database, which processes all statements and queries.
      */
     public static DBConnectivity DB;
-
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
+        
+        SetWindowsLook();
+        
         DB = new DBConnectivity();
-
-        if (DB.connect()) {
+        
+        if (DB.connect() ){
             /* Mihai testing
             -- DB createCustomer()
             Customer customer = new Customer(1, "Holder Name", "Simon", "Peterson", false, false, false, "1st Avenue of New York", "0711 112 1124", null, "simon.peterson@yahoo.com", 0, null);
             DB.createCustomer(customer);
             */          
-            LoginUI homepage = new LoginUI();
-            homepage.setVisible(true);
+        }
+        
+        
+        StaffView staffView = new StaffView();
+        staffView.tabbedContent.addTab("Job Browser", new JobBrowserPanel());
+        staffView.tabbedContent.addTab("Customer Browser", new CustomerBrowserPanel());
+        staffView.tabbedContent.addTab("Customer Profile", new CustomerProfilePanel());
+        staffView.setVisible(true);
+    }
+    
+    private static void SetWindowsLook()
+    {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Windows".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(LoginView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(LoginView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(LoginView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(LoginView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
     }
 }
